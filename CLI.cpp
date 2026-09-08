72fe2f97b0e9af25d4de8be5acc7843a1c483d84
Mads Thybaek-Hansen <nickygivskud@gmail.com>
2026-09-08 13:54:59 +0200

    forgot to commit this

--- CLI.cpp
+++ CLI.cpp
@@ -1,7 +1,18 @@
+#include "include/CLI.h"
+#include <iostream>
+
+CLI::CLI(const std::map<std::string, std::string>& countryList): countrylist(countryList) {};
+
+CLI::~CLI() {
+    // Destructor implementation (if needed)
+}
 
 
+void CLI::printCoulist() {
 
+    std::cout << "Country List:" << std::endl;
 
-void printCoulist() {
-    
+    for (const auto& pair : countrylist) {
+        std::cout << "Country: " << pair.first << ", Code: " << pair.second << std::endl;
+    }
 }
